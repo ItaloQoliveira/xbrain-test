38470544150830d10471b261f819c6acd94c5315
“Italo <italo.queiroz@ykt.com.br>
2022-03-08 21:27:26 -0300

    add: initial project structure

---
+++ xbrain-project/src/main/java/com/br/xbrainproject/XbrainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.br.xbrainproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrainProjectApplication.class, args);
+	}
+
+}
---
+++ xbrain-project/src/test/java/com/br/xbrainproject/XbrainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.xbrainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbrainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
